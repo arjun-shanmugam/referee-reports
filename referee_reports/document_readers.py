2387ff7c45446f1d5d52bf53f9679bb58ca7d912
arjun-shanmugam <65803362+arjun-shanmugam@users.noreply.github.com>
2023-06-19 12:19:13 -0400

    Renamed rec column to recommendation

--- referee_reports/document_readers.py
+++ referee_reports/document_readers.py
@@ -278,7 +278,7 @@ class ReportReader(JournalDocumentReader):
     def _merge_referee_characteristics(self):
         referee_characteristics_df = pd.read_csv(self._referee_characteristics_file, index_col=['paper_num', 'num'])
         referee_characteristics_df.index = referee_characteristics_df.index.rename(['paper', 'num'])
-        referee_characteristics_df = referee_characteristics_df.rename(columns={'rec', 'recommendation'})
+        referee_characteristics_df = referee_characteristics_df.rename(columns={'rec': 'recommendation'})
 
         # Remove any trailing/leading whitespace in the recommendation or decision columns.
         referee_characteristics_df.loc[:, 'recommendation'] = referee_characteristics_df['recommendation'].str.strip()
