8faa1637c231d5b26ac2b07fa1d02e01cbd5e509
arjun-shanmugam <65803362+arjun-shanmugam@users.noreply.github.com>
2023-06-21 10:07:50 -0400

    Made male and female histograms different colors when plotting histograms of report lengths separately by gender

--- referee_reports/referee_report_dataset.py
+++ referee_reports/referee_report_dataset.py
@@ -10,6 +10,7 @@ from stargazer.stargazer import Stargazer
 import matplotlib.pyplot as plt
 import numpy as np
 
+import constants
 from referee_reports.constants import Colors
 from referee_reports.pkldir.decode import decode
 import pandas as pd
@@ -318,7 +319,7 @@ class RefereeReportDataset:
 
         # Plot distribution of report lengths, separately by gender.
         fig, (ax1, ax2) = plt.subplots(1, 2, sharey=True)
-        for gender, title, ax in zip([1, 0], ["Written by Female Referee", "Written by Non-female Referee"], [ax1, ax2]):
+        for gender, title, ax, color in zip([1, 0], ["Written by Female Referee", "Written by Non-female Referee"], [ax1, ax2], constants.Colors.P1, constants.Colors.P2):
             report_lengths = self._df.loc[self._df['_female_'] == gender, self._reports_vocabulary].sum(axis=1)
             plot_histogram(ax, x=report_lengths, title=title, xlabel="Length")
         save_figure_and_close(fig, os.path.join(self._output_directory, "histogram_report_lengths_by_gender.png"))
